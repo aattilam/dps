b71b9894a6f6e272a06372b25a476700dbd2b063
AttilaM <59540584+aattilam@users.noreply.github.com>
2024-06-28 18:24:17 +0200

    Update dconf.sh

--- dconf.sh
+++ dconf.sh
@@ -28,3 +28,4 @@ done
 dconf load / < ~/.config/gnome-settings.conf
 gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' && gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 rm -f ~/.config/gnome-settings.conf ~/.config/autostart-scripts/dconf.sh &
+gnome-session-quit --logout --no-prompt
